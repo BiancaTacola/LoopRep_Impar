cb18f5a8ecce86c2498e4b671ff919b603fd89bf
Bianca S. Tacola <95148673+BiancaTacola@users.noreply.github.com>
2023-03-29 14:57:02 -0300

    Add files via upload
    """Esse código é um loop de repetição que utiliza a função "range" para gerar uma sequência de números impares de 1 a 1000
    e, em seguida, imprime cada"""

---
+++ Loop_Rep_Impar.py
@@ -0,0 +1,6 @@
+"""Esse código é um loop de repetição que utiliza a função "range" para gerar uma sequência de números impares
+ de 1 a 1000
+e, em seguida, imprime cada"""
+
+for x in range(1,1001,2):
+    print(x)
